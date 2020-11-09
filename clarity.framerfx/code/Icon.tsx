900488e9b08cd1e601c56dccff9ca2e7368a7cf0
iKettles <iain@framer.com>
2020-11-09 12:57:13 +0100

    Clean up Icon component, add enum control for setting icon name

--- clarity.framerfx/code/Icon.tsx
+++ clarity.framerfx/code/Icon.tsx
@@ -1,24 +1,31 @@
 import * as React from "react"
 import { addPropertyControls, ControlType } from "framer"
 import { Icon as Icon_ } from "../../../clarity-react/dist/icon/Icon"
+import { AllShapes } from "../../../clarity-react/node_modules/@clr/icons/shapes/all-shapes"
 
 export function Icon(props) {
-    let styleProp = props.style ? "" : "is-solid"
-    let classes = `${styleProp} ${props.status}`
-    return <Icon_ shape={props.shape} className={classes} size={props.size} />
+    return (
+        <Icon_
+            shape={props.shape}
+            className={`${props.style ? "" : "is-solid"} ${props.status}`}
+            size={props.size}
+        />
+    )
 }
 
 Icon.defaultProps = {
     shape: "home",
-    size: 36,
+    size: "36",
     width: 36,
     height: 36,
 }
 
 addPropertyControls(Icon, {
     shape: {
-        type: ControlType.String,
-        title: "Icon name",
+        type: ControlType.Enum,
+        options: Object.keys(AllShapes).sort(),
+        title: "Name",
+        defaultValue: "home",
     },
     style: {
         type: ControlType.Boolean,
