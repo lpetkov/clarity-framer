4afd4173340f59a4945e2c71337b7ea3698c1756
iKettles <iain@framer.com>
2020-11-09 13:22:42 +0100

    Add SignPost to Icon component

--- clarity.framerfx/code/Icon.tsx
+++ clarity.framerfx/code/Icon.tsx
@@ -1,16 +1,73 @@
 import * as React from "react"
 import { addPropertyControls, ControlType } from "framer"
+import {
+    SignPost,
+    SignPostDirection,
+} from "../../../clarity-react/dist/forms/signpost/SignPost"
 import { Icon as Icon_ } from "../../../clarity-react/dist/icon/Icon"
 import { AllShapes } from "../../../clarity-react/node_modules/@clr/icons/shapes/all-shapes"
+import { placeholderStyle } from "./utils/placeholder"
+import { useConnectedComponentInstance } from "./utils/useConnectedComponentInstance"
 
 export function Icon(props) {
-    return (
-        <Icon_
-            shape={props.shape}
-            className={`${props.style ? "" : "is-solid"} ${props.status}`}
-            size={props.size}
-        />
+    const {
+        shape,
+        style,
+        status,
+        size,
+        signPostContentType,
+        signPostText,
+        signPostContent,
+        signPostDirection,
+    } = props
+    const [connectedSignPostContent] = useConnectedComponentInstance(
+        signPostContent
     )
+    const content = React.useMemo(() => {
+        if (signPostContentType === "text") {
+            return signPostText
+        }
+
+        if (!connectedSignPostContent) {
+            return (
+                <div style={{ ...placeholderStyle }}>
+                    Connect a layer on the canvas to be displayed here.
+                </div>
+            )
+        }
+        const contentHeight = (connectedSignPostContent as any).props.height
+        const contentWidth = (connectedSignPostContent as any).props.width
+        return (
+            <div
+                style={{
+                    position: "relative",
+                    height: contentHeight,
+                    width: contentWidth,
+                }}
+            >
+                {signPostContent}
+            </div>
+        )
+    }, [signPostContentType, signPostText, connectedSignPostContent])
+    const icon = React.useMemo(() => {
+        return (
+            <Icon_
+                shape={shape}
+                className={`${style ? "" : "is-solid"} ${status}`}
+                size={size}
+            />
+        )
+    }, [shape, size, style, status])
+
+    if (props.signPost) {
+        return (
+            <SignPost direction={signPostDirection} openAt={icon}>
+                {content}
+            </SignPost>
+        )
+    }
+
+    return icon
 }
 
 Icon.defaultProps = {
@@ -18,7 +75,12 @@ Icon.defaultProps = {
     size: "36",
     width: 36,
     height: 36,
+    signPost: false,
+    signPostContentType: "text",
+    signPostText: "This is some example SignPost content",
 }
+
+const sortedSignPostDirectionKeys = Object.keys(SignPostDirection).sort()
 
 addPropertyControls(Icon, {
     shape: {
@@ -60,5 +122,41 @@ addPropertyControls(Icon, {
             "Warning",
             "Info",
         ],
+    },
+    signPost: {
+        type: ControlType.Boolean,
+        title: "SignPost",
+        defaultValue: Icon.defaultProps.signPost,
+    },
+    signPostContentType: {
+        title: "Content",
+        type: ControlType.Enum,
+        options: ["text", "component"],
+        optionTitles: ["Text", "Custom"],
+        displaySegmentedControl: true,
+        defaultValue: Icon.defaultProps.signPostContentType,
+        hidden: (props) => !props.signPost,
+    },
+    signPostText: {
+        title: "Text",
+        type: ControlType.String,
+        defaultValue: Icon.defaultProps.signPostText,
+        hidden: (props) =>
+            !(props.signPost && props.signPostContentType === "text"),
+    },
+    signPostContent: {
+        title: "Layer",
+        type: ControlType.ComponentInstance,
+        hidden: (props) =>
+            !(props.signPost && props.signPostContentType === "component"),
+    },
+    signPostDirection: {
+        title: "Direction",
+        type: ControlType.Enum,
+        optionTitles: sortedSignPostDirectionKeys,
+        options: sortedSignPostDirectionKeys.map(
+            (key) => SignPostDirection[key]
+        ),
+        hidden: (props) => !props.signPost,
     },
 })
