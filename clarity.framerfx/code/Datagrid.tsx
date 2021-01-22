0fd045ed5bbda3a592fa2cea645d13ed2f2f6ecc
iKettles <iain@framer.com>
2021-01-22 11:12:12 +0100

    Clean up DataGrid component

--- clarity.framerfx/code/Datagrid.tsx
+++ clarity.framerfx/code/Datagrid.tsx
@@ -1,45 +1,13 @@
 import * as React from "react"
 import { Frame, addPropertyControls, ControlType } from "framer"
 import axios from "axios"
-//@ts-ignore
 import { DataGrid as DataGrid_ } from "../../../clarity-react/dist/datagrid/Datagrid"
-
 import {
     GridSelectionType,
     GridRowType,
-    SortOrder,
-    DataGridFilter,
-    FilterType,
-    //@ts-ignore
 } from "../../../clarity-react/dist/datagrid/Datagrid"
-
-import {
-    normalColumns,
-    normalRows,
-    customRows,
-    GridActions,
-    sortColumns,
-    expandableRows,
-    filterFunction,
-    sortFunction,
-    paginationDetails,
-    paginationRows,
-    pageFilterFunction,
-    hideableColumns,
-    //@ts-ignore
-} from "../../../clarity-react/dist/datagrid/DataGridValues"
-
-// import { normalRows_ } from "./testData.json"
-//@ts-ignore
-import { CustomFilter } from "../../../clarity-react/dist/datagrid/CustomFilter"
-
-const datagridRef = React.createRef<DataGrid_>()
-const datagridActionsRef = React.createRef<GridActions>()
-const datagridFilterRef = React.createRef<DataGrid_>()
-const datagridFilterSortRef = React.createRef<DataGrid_>()
-const datagridCustomFilterRef = React.createRef<DataGrid_>()
-const datagridFullDemoRef = React.createRef<DataGrid_>()
-const filterRef = React.createRef<DataGridFilter>()
+import { paginationDetails } from "../../../clarity-react/dist/datagrid/DataGridValues"
+import { indentPropertyControlTitle } from "./utils/propertyControls"
 
 export function Datagrid(props) {
     const {
@@ -47,62 +15,35 @@ export function Datagrid(props) {
         customFooterString,
         hasFooter,
         hasPagination,
+        dataUrl,
         ...rest
     } = props
     const [rowsData, setRowsData] = React.useState([])
 
-    // Fetch rows data
     React.useEffect(() => {
         const fetchData = async () => {
-            const result = await axios(props.rowsUrl)
+            const result = await axios(dataUrl)
             setRowsData(result.data)
         }
         fetchData()
-    }, [])
-
-    // Generate columns
-    var columnArr = []
-    let numberOfColumns = props.columnItems.length
-
-    for (var i = 0; i < numberOfColumns; i++) {
-        let columnTitle = props.columnItems[i]
-        columnArr.push({
-            columnName: `${columnTitle}`,
-        })
-    }
-
-    // Footer
-    let footerData
-    if (hasCustomFooter) {
-        footerData = customFooterString
-    }
-    const footer = {
-        footerData: footerData,
-        showFooter: hasFooter,
-    }
-
-    // Pagination
-
-    let pagination
-    if (hasPagination) {
-        pagination = paginationDetails
-    }
-
-    const expandable = props.expandable ? GridRowType.EXPANDABLE : null
-    const selection = props.hasSelection ? props.selectionType : null
+    }, [dataUrl])
 
     return (
         <Frame size="100%" background={null}>
             <DataGrid_
-                {...props}
-                ref={datagridRef}
-                columns={columnArr}
+                {...rest}
+                columns={props.columnItems.map((column) => ({
+                    columnName: column,
+                }))}
                 rows={rowsData}
-                footer={footer}
-                selectionType={selection}
-                rowType={expandable}
+                footer={{
+                    footerData: hasCustomFooter ? customFooterString : null,
+                    showFooter: hasFooter,
+                }}
+                selectionType={props.hasSelection && props.selectionType}
+                rowType={props.expandable && GridRowType.EXPANDABLE}
                 style={{ height: "100%" }}
-                pagination={pagination}
+                pagination={hasPagination && paginationDetails}
                 // onRowSelect={() => {
                 //     const rows = datagridRef.current!.getSelectedRows()
                 //     datagridActionsRef.current!.updateActions(rows)
@@ -125,9 +66,9 @@ Datagrid.defaultProps = {
 }
 
 addPropertyControls(Datagrid, {
-    rowsUrl: {
+    dataUrl: {
         type: ControlType.File,
-        title: "Rows data",
+        title: "Data (JSON)",
         allowedFileTypes: ["json"],
     },
     columnItems: {
@@ -142,36 +83,35 @@ addPropertyControls(Datagrid, {
     },
     hasFooter: {
         type: ControlType.Boolean,
-        title: "Footer",
+        title: "Show Footer",
         enabledTitle: "Yes",
         disabledTitle: "No",
     },
     hasCustomFooter: {
         type: ControlType.Boolean,
         defaultValue: false,
-        title: "Custom footer",
-        enabledTitle: "Yes",
-        disabledTitle: "No",
+        title: "Footer",
+        enabledTitle: "Custom",
+        disabledTitle: "Default",
     },
     customFooterString: {
         type: ControlType.String,
-        title: "Footer text",
+        title: indentPropertyControlTitle("Text"),
         defaultValue: "Custom footer text",
         placeholder: "Footer text",
-        hidden(props) {
-            return props.customFilter === false
-        },
+        hidden: (props) => !props.hasCustomFooter,
     },
     hasSelection: {
         type: ControlType.Boolean,
-        title: "Has selection",
+        title: "Selection",
         defaultValue: false,
-        enabledTitle: "Yes",
-        disabledTitle: "No",
+        enabledTitle: "Show",
+        disabledTitle: "Hide",
     },
     selectionType: {
-        type: ControlType.SegmentedEnum,
-        title: "Custom footer",
+        type: ControlType.Enum,
+        displaySegmentedControl: true,
+        title: "Selection Type",
         options: [GridSelectionType.SINGLE, GridSelectionType.MULTI],
         optionTitles: ["Radio", "Checkbox"],
         defaultValue: GridSelectionType.MULTI,
